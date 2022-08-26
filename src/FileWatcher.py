82c4dbbcd2977c36a2ebd6a9f11897a6ec2e8b2c
xbbnr82 <xbbnr82@PRAMALINGAM2W10>
2022-08-26 11:08:35 +0530

    Added props and filewatcher changes

--- src/FileWatcher.py
+++ src/FileWatcher.py
@@ -1,8 +1,8 @@
-
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import os
+import sys
 from datetime import datetime
 from pathlib import Path
 import configparser
@@ -40,17 +40,34 @@ class Handler(FileSystemEventHandler):
        
         else:
             print ("Received event - %s, path - %s" %(event.event_type, event.src_path))
-            print("File size - %s" % os.path.getsize(event.src_path))
-            print("File create time - %s" % datetime.fromtimestamp(os.path.getctime(event.src_path)).strftime('%Y-%m-%d %H:%M:%S'))
-            print("File modified time - %s" % datetime.fromtimestamp(os.path.getmtime(event.src_path)).strftime('%Y-%m-%d %H:%M:%S'))
             print("File Name - %s" % Path(event.src_path).stem)
             print("File Name - %s" % Path(event.src_path).suffix.replace('.',''))
+            if os.path.exists(event.src_path):
+                print("File size - %s" % os.path.getsize(event.src_path))
+                print("File create time - %s" % datetime.fromtimestamp(os.path.getctime(event.src_path)).strftime('%Y-%m-%d %H:%M:%S'))
+                print("File modified time - %s" % datetime.fromtimestamp(os.path.getmtime(event.src_path)).strftime('%Y-%m-%d %H:%M:%S'))
+
 
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
-    config.read("C:\\Users\\xbbnr82\\Desktop\\Hackathon\\props\\settings.ini")
-    g_pathInbound = config['watcher']['inboundfolder']
-    g_pathArchive = config['watcher']['archive']
+    curDirPath = os.path.dirname(os.path.realpath(__file__))
+    iniPath = curDirPath + "\\..\\props\\settings.ini"
+    dataset = config.read(iniPath)
+    
+    if len(dataset) != 1:
+        print("Failed to open/find config file")
+        sys.exit()
+    
+    g_pathInbound = config['watcher']['inboundfolder'].strip('\"')
+    if not os.path.exists(g_pathInbound):
+        print("Inbound Directory not exists")
+        sys.exit()
+        
+    g_pathArchive = config['watcher']['archive'].strip('\"')
+    if not os.path.exists(g_pathArchive):
+        print("Arcive Directory not exists")
+        sys.exit()
+        
     w = Watcher()
     w.run()
