c532939dc6dcd6babe28336a356ca5d5ce29b7db
xbbnr82 <xbbnr82@PRAMALINGAM2W10>
2022-08-26 10:34:50 +0530

    Added watcher src

---
+++ src/FileWatcher.py
@@ -0,0 +1,56 @@
+
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import os
+from datetime import datetime
+from pathlib import Path
+import configparser
+
+g_pathInbound = ""
+g_pathArchive = ""
+
+class Watcher:
+    pathToWatch = ""
+    
+    def __init__(self):
+        self.pathToWatch = str(g_pathInbound)
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.pathToWatch, recursive=True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print ("Error")
+
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+       
+        else:
+            print ("Received event - %s, path - %s" %(event.event_type, event.src_path))
+            print("File size - %s" % os.path.getsize(event.src_path))
+            print("File create time - %s" % datetime.fromtimestamp(os.path.getctime(event.src_path)).strftime('%Y-%m-%d %H:%M:%S'))
+            print("File modified time - %s" % datetime.fromtimestamp(os.path.getmtime(event.src_path)).strftime('%Y-%m-%d %H:%M:%S'))
+            print("File Name - %s" % Path(event.src_path).stem)
+            print("File Name - %s" % Path(event.src_path).suffix.replace('.',''))
+
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read("C:\\Users\\xbbnr82\\Desktop\\Hackathon\\props\\settings.ini")
+    g_pathInbound = config['watcher']['inboundfolder']
+    g_pathArchive = config['watcher']['archive']
+    w = Watcher()
+    w.run()
