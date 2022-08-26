45ec21d3ef2714a6b157dff18b61b85677177d60
xbbnr82 <xbbnr82@PRAMALINGAM2W10>
2022-08-26 13:44:13 +0530

    Added DBManagement and Filewatcher changes

---
+++ src/DBManagement.py
@@ -0,0 +1,46 @@
+import os
+from datetime import datetime
+import sqlite3
+import atexit
+import sys
+
+g_dbCon = sqlite3.Connection
+
+def createTable():
+     con = sqlite3.connect(g_dbPath)
+     cur = con.cursor()
+     cur.execute("CREATE TABLE IncomingFileMetaData(fileName, arrivalTime, size, sender, noOfRecords)")
+     cur.execute("CREATE TABLE FileAlerts(fileName, alertName, alertDesc)")
+     cur.execute("CREATE TABLE AlertDetials(alertName, alertInterestedParty)")
+     con.commit()
+     
+     
+def insertAlertInterestedParty(alertName, alertParty):
+    cur = g_dbCon.cursor()
+    cur.execute("INSERT INTO AlertDetials VALUES(?, ?)", (alertName, alertParty))
+    g_dbCon.commit()
+    
+def exit_handler():
+    print ('My application is ending!')
+    g_dbCon.close()
+     
+if __name__ == '__main__':
+    atexit.register(exit_handler)
+    curDirPath = os.path.dirname(os.path.realpath(__file__))
+    g_dbPath = curDirPath + "\\..\\db\\ddm.db"
+     
+    if not os.path.exists(g_dbPath):
+       createTable()
+       
+    g_dbCon = sqlite3.connect(g_dbPath)
+       
+    print("\nAdd Alert interested party\n")
+    
+    addMore = 'Y'
+    while addMore == 'Y':
+        alertName = input("Alert Name:")
+        alertParty = input("Party:")
+        insertAlertInterestedParty(alertName, alertParty)
+        addMore = input("Add more records (Y/N): ")
+        
+    g_dbCon.close()
--- src/FileWatcher.py
+++ src/FileWatcher.py
@@ -6,13 +6,17 @@ import sys
 from datetime import datetime
 from pathlib import Path
 import configparser
+import sqlite3
+import pandas as pd
 
 g_pathInbound = ""
 g_pathArchive = ""
+g_dbPath = ""
+g_dbCon = sqlite3.Connection
 
 class Watcher:
     pathToWatch = ""
-    
+
     def __init__(self):
         self.pathToWatch = str(g_pathInbound)
         self.observer = Observer()
@@ -29,25 +33,43 @@ class Watcher:
             print ("Error")
 
         self.observer.join()
-
-
+        
+def insertIncomingFileMetaData(fileName, fileArrivalTime, fileSize, fileSender, fileNoOfRecords):
+    cur = g_dbCon.cursor()
+    cur.execute("INSERT INTO IncomingFileMetaData VALUES(?, ?, ?, ?, ?)", (fileName, fileArrivalTime, fileSize, fileSender, fileNoOfRecords))
+    g_dbCon.commit()
+    
 class Handler(FileSystemEventHandler):
-
     @staticmethod
     def on_any_event(event):
         if event.is_directory:
             return None
-       
         else:
             print ("Received event - %s, path - %s" %(event.event_type, event.src_path))
-            print("File Name - %s" % Path(event.src_path).stem)
-            print("File Name - %s" % Path(event.src_path).suffix.replace('.',''))
-            if os.path.exists(event.src_path):
-                print("File size - %s" % os.path.getsize(event.src_path))
-                print("File create time - %s" % datetime.fromtimestamp(os.path.getctime(event.src_path)).strftime('%Y-%m-%d %H:%M:%S'))
-                print("File modified time - %s" % datetime.fromtimestamp(os.path.getmtime(event.src_path)).strftime('%Y-%m-%d %H:%M:%S'))
-
-
+            if event.event_type == "created" and os.path.exists(event.src_path):
+                fileName = Path(event.src_path).stem
+                fileExt = Path(event.src_path).suffix.replace('.','')
+                fileSize = os.path.getsize(event.src_path)
+                fileArrivalTime = datetime.fromtimestamp(os.path.getctime(event.src_path)).strftime('%Y%m%d%H%M%S')
+                fileNameSplit = fileName.split('_')[-1]
+                fileNoOfRecords = 0
+                
+                if len(fileNameSplit) == len(fileName):
+                    fileSender = "DEFAULT"
+                else:
+                    fileSender = fileNameSplit
+                
+                if fileExt.lower() == "csv":
+                    fileNoOfRecords = len(pd.read_csv(event.src_path).index)
+                    
+                print("File name - %s" % fileName)
+                print("File arrival time - %s" % fileArrivalTime)
+                print("File size - %s" % fileSize)
+                print("File sender - %s" % fileSender)
+                print("File no of records - %s" % fileNoOfRecords)
+                
+                insertIncomingFileMetaData(fileName, fileArrivalTime, fileSize, fileSender, fileNoOfRecords)
+                
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
@@ -69,5 +91,13 @@ if __name__ == '__main__':
         print("Arcive Directory not exists")
         sys.exit()
         
+    g_dbPath = curDirPath + "\\..\\db\\ddm.db"
+    
+    if not os.path.exists(g_dbPath):
+       print("DB not exists.")
+       sys.exit()
+       
+    g_dbCon = sqlite3.connect(g_dbPath)
+    
     w = Watcher()
     w.run()
